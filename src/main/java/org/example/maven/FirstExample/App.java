0296d01a022b6578d10c2e67d5398a297d3efab9
pablochrun <p.covarrubias.h@gmail.com>
2017-02-04 00:35:05 +0100

    Initial commit

---
+++ src/main/java/org/example/maven/FirstExample/App.java
@@ -0,0 +1,17 @@
+package org.example.maven.FirstExample;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Gson gson = new Gson();
+    }
+}
