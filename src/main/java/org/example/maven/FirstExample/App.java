b194c050dbbcfce5c6c9e2b343056c877525d6bc
Pablo Covarrubias Herrera <pablo.covarrubias@altran.es>
2017-02-08 16:51:29 +0100

    update from altran

--- src/main/java/org/example/maven/FirstExample/App.java
+++ src/main/java/org/example/maven/FirstExample/App.java
@@ -11,6 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! third commit from posh-git" );
+		System.out.println( "ey, commit from altran" );
                 
     }
 }
